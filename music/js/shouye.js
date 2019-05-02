b3e718ddc0650ee56067b4230d2e770f23ba3955
applelizihao <2184724790@qq.com>
2019-05-02 15:24:07 +0800

    new

--- music/js/shouye.js
+++ music/js/shouye.js
@@ -1,19 +1,51 @@
+Vue.component('player-song', {
+    props: ['playsong'],
+    template: `
+    <div>
+    <div class="marginplayer"></div>
+    <div class="player">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="song-img col-xs-2 col-sm-1 col-md-1 col-lg-1">
+                    <img src="https://ss1.baidu.com/6ONXsjip0QIZ8tyhnq/it/u=2034093601,2591364475&fm=58&bpow=400&bpoh=583"
+                        alt="">
+                </div>
+                <div class="song-info col-xs-5 col-sm-9 col-md-9 col-lg-9">
+                    <p class="song-name">每一句都很甜</p>
+                    <p class="song-author">新乐城府</p>
+                </div>
+                <div class="console col-xs-5 col-sm-2 col-md-2 col-lg-2">
+                    <i class="play glyphicon glyphicon-play"> </i>
+                    <i class="song-list glyphicon glyphicon-signal"></i>
+                </div>
+            </div>
+        </div>
+        <audio autoplay :src="playsong"></audio>
+    </div>
+    </div>
+    `
+})
+
+//获取排行榜音乐组件
 Vue.component('header-song', {
     props: ['id'],
     data: function () {
         return {
             imgsrc: '',
             listName: '',
-            updateTime: ''
+            updateTime: '',
+            songList: ''
         }
     },
     created() {
         fetch(`https://music.niubishanshan.top/api/v2/music/songList/${this.id}`)
             .then(response => response.json())
             .then(result => {
+                this.songList = result.data.songList
                 this.imgsrc = result.data.topInfo.picAlbum
                 this.listName = result.data.topInfo.listName
                 this.updateTime = result.data.updateTime
+                console.log(this.songList)
             })
     },
     template: `
@@ -30,9 +62,15 @@ Vue.component('header-song', {
     </div>
     <div class="container-fluid">
         <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        123
-        </div>
+            <li v-on:click="$emit('get',item.songMid)" v-for="(item,index) in songList">
+            <div class="song-left col-xs-2 col-sm-2 col-md-2 col-lg-2">
+               <h5>{{index+1}}</h5>
+            </div>
+            <div class="song-right col-xs-10 col-sm-10 col-md-10 col-lg-10">
+               <span>{{item.songName}}</span>
+                <span>{{item.singer[0].singerName}}</span>
+            </div>
+            </li>
         </div>
     </div>
 </div>
@@ -108,14 +146,18 @@ Vue.component('music', {
     </div>
 </div>
 `,
-
 })
+
+
 Vue.component('find', {
     template: '<li>find</li>'
 })
 var app = new Vue({
     el: '#app',
     data: {
+        playsong: {
+            src: ""
+        },
         songtop: {
             id: '4' //默认值空
         },
@@ -133,13 +175,28 @@ var app = new Vue({
             headersong: true //默认值false
         }
     },
+    
     methods: {
+        //排行榜点击歌曲播放
+        clickchosesongli: function (index) {
+            //赋值传过来的index
+            console.log(index)
+            fetch(`https://music.niubishanshan.top/api/v2/music/songUrllist/${index}`)
+                .then(response => response.json())
+                .then(result => {
+                    this.playsong.src = result.data[0]
+                })
+            // https://music.niubishanshan.top/api/v2/music/songUrllist/
+        },
+
+        //进入排行榜页面，并隐藏其他页面
         clicksongfn: function (index) {
             this.songtop.id = index
             this.tabconsole.component = false
             this.tabconsole.header = false
             this.tabconsole.headersong = true
         },
+
         //activeheaderlist选择
         chosefn: function (index) {
             switch (index) {

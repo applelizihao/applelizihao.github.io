400a7ca2b0c8ca788e17b0c9e08cb37ce2eba3a9
applelizihao <2184724790@qq.com>
2019-05-02 11:23:20 +0800

    更新排行榜

--- music/js/shouye.js
+++ music/js/shouye.js
@@ -15,7 +15,8 @@ Vue.component('my', {
 Vue.component('music', {
     data: function () {
         return {
-            slider: null
+            slider: null,   //获取轮播图图片
+            top:null    //获取排行榜
         }
     },
     created() {
@@ -23,10 +24,18 @@ Vue.component('music', {
             .then(response => response.json())
             .then(result => {
                 this.slider = result.data.slider
-                console.log(this.slider)
+                // console.log(this.slider)
+            })
+        fetch('https://music.niubishanshan.top/api/v2/music/toplist')
+            .then(response => response.json())
+            .then(result => {
+                this.top=result.data
+                console.log(this.top)
             })
     },
-    template: `<div id="carousel-id" class="carousel slide" data-ride="carousel">
+    template: `
+    <div>
+    <div id="carousel-id" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
         <li :class="[index===0?'active':'']" data-target="#carousel-id" :data-slide-to="index" v-for="(item,index) in slider"></li>    
     </ol>
@@ -41,7 +50,28 @@ Vue.component('music', {
     </div>
     <a class="left carousel-control" href="#carousel-id" data-slide="prev"><span class="glyphicon glyphicon-chevron-left"></span></a>
     <a class="right carousel-control" href="#carousel-id" data-slide="next"><span class="glyphicon glyphicon-chevron-right"></span></a>
-</div>`
+</div>
+    <div class="content">
+    <div class="container-fluid">
+    <div class="row">
+        <li class="col-xs-12 col-sm-6 col-md-6 col-lg-6" v-for="(item,index) in top">
+        <h3>{{item.title}}</h3>
+            <div class="row">
+            <span :songid="item." v-on:click="clicksongfn" class="col-xs-12 col-sm-12 col-md-12 col-lg-12" v-for="(item,index) in item.songList">{{item.songName}}
+            <p class="singername">{{item.singerName}}</p>
+            </span>
+            </div>
+            </li>
+   </div>
+        </div>
+    </div>
+</div>
+`,
+methods: {
+    clicksongfn:function () { 
+        alert('xxx')
+     }
+},
 })
 Vue.component('find', {
     template: '<li>find</li>'
